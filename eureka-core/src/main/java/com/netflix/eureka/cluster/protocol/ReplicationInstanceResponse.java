c1f1694b8b03c0b5f89bc30a8d6d85cb3753b7ae
nimakarimipour <karimipour.nima@gmail.com>
2022-04-07 07:42:10 -0700

    Final Result

--- eureka-core/src/main/java/com/netflix/eureka/cluster/protocol/ReplicationInstanceResponse.java
+++ eureka-core/src/main/java/com/netflix/eureka/cluster/protocol/ReplicationInstanceResponse.java
@@ -1,5 +1,6 @@
 package com.netflix.eureka.cluster.protocol;
 
+import javax.annotation.Nullable;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.netflix.appinfo.InstanceInfo;
@@ -10,12 +11,12 @@ import com.netflix.appinfo.InstanceInfo;
 public class ReplicationInstanceResponse {
 
     private final int statusCode;
+
+    @Nullable
     private final InstanceInfo responseEntity;
 
     @JsonCreator
-    public ReplicationInstanceResponse(
-            @JsonProperty("statusCode") int statusCode,
-            @JsonProperty("responseEntity") InstanceInfo responseEntity) {
+    public ReplicationInstanceResponse(@JsonProperty("statusCode") int statusCode, @JsonProperty("responseEntity") @Nullable InstanceInfo responseEntity) {
         this.statusCode = statusCode;
         this.responseEntity = responseEntity;
     }
@@ -24,6 +25,7 @@ public class ReplicationInstanceResponse {
         return statusCode;
     }
 
+    @Nullable
     public InstanceInfo getResponseEntity() {
         return responseEntity;
     }
@@ -34,14 +36,11 @@ public class ReplicationInstanceResponse {
             return true;
         if (o == null || getClass() != o.getClass())
             return false;
-
         ReplicationInstanceResponse that = (ReplicationInstanceResponse) o;
-
         if (statusCode != that.statusCode)
             return false;
         if (responseEntity != null ? !responseEntity.equals(that.responseEntity) : that.responseEntity != null)
             return false;
-
         return true;
     }
 
@@ -55,6 +54,8 @@ public class ReplicationInstanceResponse {
     public static final class Builder {
 
         private int statusCode;
+
+        @Nullable
         private InstanceInfo responseEntity;
 
         public Builder setStatusCode(int statusCode) {
