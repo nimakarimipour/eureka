fba9bab74b1a40029c6d0ad4cf470fe0caca9fe5
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-30 18:24:58 -0700

    fix: 24 - DEREFERENCE_NULLABLE - existingLease.getHolder().getStatus().name(), - dereferenced expression existingLease is @Nullable

--- eureka-core/src/main/java/com/netflix/eureka/registry/rule/LeaseExistsRule.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/rule/LeaseExistsRule.java
@@ -2,6 +2,7 @@ package com.netflix.eureka.registry.rule;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.eureka.lease.Lease;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +35,7 @@ public class LeaseExistsRule implements InstanceStatusOverrideRule {
               || InstanceInfo.InstanceStatus.UP.equals(existingStatus))) {
         logger.debug(
             "There is already an existing lease with status {}  for instance {}",
-            existingLease.getHolder().getStatus().name(),
+            Nullability.castToNonnull(existingLease, "reason...").getHolder().getStatus().name(),
             existingLease.getHolder().getId());
         return StatusOverrideResult.matchingStatus(existingLease.getHolder().getStatus());
       }
