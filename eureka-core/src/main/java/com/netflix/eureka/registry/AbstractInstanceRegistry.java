8be994342e06dd92885e41275daddad1bd964795
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 18:01:10 -0700

    Revert "fix: 19 - METHOD_NO_INIT - protected AbstractInstanceRegistry( - initializer method does not guarantee @NonNull field responseCache (line 111) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit c042b6894c5f5359615489bd8b135e2cfe206148.

--- eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
@@ -125,10 +125,6 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
         getDeltaRetentionTask(),
         serverConfig.getDeltaRetentionTimerIntervalInMs(),
         serverConfig.getDeltaRetentionTimerIntervalInMs());
-
-    this.responseCache =
-        new ResponseCacheImpl(
-            serverConfig, serverCodecs, this); // Ensure responseCache is initialized
   }
 
   @Override
