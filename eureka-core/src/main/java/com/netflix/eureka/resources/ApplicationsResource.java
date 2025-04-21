c3389e01203d94e6a619cdd8745689e095be58b5
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 18:00:11 -0700

    Revert "fix: 9 - DEREFERENCE_NULLABLE - regions = regionsStr.toLowerCase().split(","); - dereferenced expression regionsStr is @Nullable"
    This reverts commit 864e56cfa5658e73b39b9cb24ea96c0614666934.

--- eureka-core/src/main/java/com/netflix/eureka/resources/ApplicationsResource.java
+++ eureka-core/src/main/java/com/netflix/eureka/resources/ApplicationsResource.java
@@ -114,18 +114,23 @@ public class ApplicationsResource {
       @HeaderParam(HEADER_ACCEPT_ENCODING) String acceptEncoding,
       @HeaderParam(EurekaAccept.HTTP_X_EUREKA_ACCEPT) String eurekaAccept,
       @Context UriInfo uriInfo,
-      @QueryParam("regions") String regionsStr) {
+      @Nullable @QueryParam("regions") String regionsStr) {
 
-    boolean isRemoteRegionRequested = regionsStr != null && !regionsStr.isEmpty();
+    boolean isRemoteRegionRequested = null != regionsStr && !regionsStr.isEmpty();
     String[] regions = null;
     if (!isRemoteRegionRequested) {
       EurekaMonitors.GET_ALL.increment();
     } else {
       regions = regionsStr.toLowerCase().split(",");
-      Arrays.sort(regions);
+      Arrays.sort(
+          regions); // So we don't have different caches for same regions queried in different
+      // order.
       EurekaMonitors.GET_ALL_WITH_REMOTE_REGIONS.increment();
     }
 
+    // Check if the server allows the access to the registry. The server can
+    // restrict access if it is not
+    // ready to serve traffic depending on various reasons.
     if (!registry.shouldAllowAccess(isRemoteRegionRequested)) {
       return Response.status(Status.FORBIDDEN).build();
     }
