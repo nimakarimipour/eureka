708da07ef456f70673777fd4c092f772ec0a05f7
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 18:00:17 -0700

    fix: 10 - DEREFERENCE_NULLABLE - regions = regionsStr.toLowerCase().split(","); - dereferenced expression regionsStr is @Nullable

--- eureka-core/src/main/java/com/netflix/eureka/resources/ApplicationsResource.java
+++ eureka-core/src/main/java/com/netflix/eureka/resources/ApplicationsResource.java
@@ -197,26 +197,22 @@ public class ApplicationsResource {
       @HeaderParam(HEADER_ACCEPT_ENCODING) String acceptEncoding,
       @HeaderParam(EurekaAccept.HTTP_X_EUREKA_ACCEPT) String eurekaAccept,
       @Context UriInfo uriInfo,
-      @Nullable @QueryParam("regions") String regionsStr) {
+      @QueryParam("regions") String regionsStr) {
 
-    boolean isRemoteRegionRequested = null != regionsStr && !regionsStr.isEmpty();
+    boolean isRemoteRegionRequested = regionsStr != null && !regionsStr.isEmpty();
 
-    // If the delta flag is disabled in discovery or if the lease expiration
-    // has been disabled, redirect clients to get all instances
     if ((serverConfig.shouldDisableDelta())
         || (!registry.shouldAllowAccess(isRemoteRegionRequested))) {
       return Response.status(Status.FORBIDDEN).build();
     }
 
     String[] regions = null;
-    if (!isRemoteRegionRequested) {
-      EurekaMonitors.GET_ALL_DELTA.increment();
-    } else {
+    if (isRemoteRegionRequested) {
       regions = regionsStr.toLowerCase().split(",");
-      Arrays.sort(
-          regions); // So we don't have different caches for same regions queried in different
-      // order.
+      Arrays.sort(regions);
       EurekaMonitors.GET_ALL_DELTA_WITH_REMOTE_REGIONS.increment();
+    } else {
+      EurekaMonitors.GET_ALL_DELTA.increment();
     }
 
     CurrentRequestVersion.set(Version.toEnum(version));
