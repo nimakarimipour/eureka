d3c37921d5818052e7af1657f375066f10d1e5c3
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 16:39:31 -0700

    fix: 3 - DEREFERENCE_NULLABLE - overriddenInstanceStatus.name(), - dereferenced expression overriddenInstanceStatus is @Nullable

--- eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
@@ -390,7 +390,8 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
         // touchASGCache(instanceInfo.getASGName());
         InstanceStatus overriddenInstanceStatus =
             this.getOverriddenInstanceStatus(instanceInfo, leaseToRenew, isReplication);
-        if (overriddenInstanceStatus == InstanceStatus.UNKNOWN) {
+        if (overriddenInstanceStatus == null
+            || overriddenInstanceStatus == InstanceStatus.UNKNOWN) {
           logger.info(
               "Instance status UNKNOWN possibly due to deleted override for instance {}"
                   + "; re-register required",
