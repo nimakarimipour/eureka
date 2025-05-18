a50cbbd6ad32a1e96af8626800d5c204580ed5cc
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 16:52:28 -0700

    Revert "fix: 2 - DEREFERENCE_NULLABLE - return isHeathly.booleanValue(); - dereferenced expression isHeathly is @Nullable"
    This reverts commit 7c93ecda4cc0e6ab6aa7b580b36b6258e79fd3e0.

--- eureka-core/src/main/java/com/netflix/eureka/util/StatusInfo.java
+++ eureka-core/src/main/java/com/netflix/eureka/util/StatusInfo.java
@@ -96,7 +96,7 @@ public class StatusInfo {
   }
 
   public boolean isHealthy() {
-    return isHeathly != null ? isHeathly.booleanValue() : false;
+    return isHeathly.booleanValue();
   }
 
   public Map<String, String> getGeneralStats() {
