3cd7b67ed90d78c04a4f11e11808f5319453e1f0
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 16:52:21 -0700

    fix: 1 - METHOD_NO_INIT - private ReplicationInstanceBuilder() {} - initializer method does not guarantee @NonNull fields appName (line 105), id (line 106), lastDirtyTimestamp (line 107), instanceInfo (line 110), action (line 111) are initialized along all control-flow paths (remember to check for exceptions or early returns).

--- eureka-core/src/main/java/com/netflix/eureka/cluster/protocol/ReplicationInstance.java
+++ eureka-core/src/main/java/com/netflix/eureka/cluster/protocol/ReplicationInstance.java
@@ -110,7 +110,13 @@ public class ReplicationInstance {
     private InstanceInfo instanceInfo;
     private Action action;
 
-    private ReplicationInstanceBuilder() {}
+    private ReplicationInstanceBuilder() {
+      this.appName = "";
+      this.id = "";
+      this.lastDirtyTimestamp = 0L;
+      this.instanceInfo = new InstanceInfo();
+      this.action = new Action();
+    }
 
     public static ReplicationInstanceBuilder aReplicationInstance() {
       return new ReplicationInstanceBuilder();
