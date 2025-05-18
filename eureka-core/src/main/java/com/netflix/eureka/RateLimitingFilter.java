2a1400112580eb0eace5f5e2415b679d0f5e5e55
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 16:52:59 -0700

    fix: 7 - METHOD_NO_INIT - public RateLimitingFilter() {} - initializer method does not guarantee @NonNull field serverConfig (line 106) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- eureka-core/src/main/java/com/netflix/eureka/RateLimitingFilter.java
+++ eureka-core/src/main/java/com/netflix/eureka/RateLimitingFilter.java
@@ -111,7 +111,9 @@ public class RateLimitingFilter implements Filter {
   }
 
   // for non-DI use
-  public RateLimitingFilter() {}
+  public RateLimitingFilter() {
+    this.serverConfig = new DefaultEurekaServerConfig();
+  }
 
   @Override
   public void init(FilterConfig filterConfig) throws ServletException {
