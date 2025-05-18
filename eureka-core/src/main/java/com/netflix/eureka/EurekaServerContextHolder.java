7016345aa2eb5fc2c99fabef9e1607f84121f85a
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 16:54:22 -0700

    fix: 22 - RETURN_NULLABLE - return holder; - returning @Nullable expression from method with @NonNull return type

--- eureka-core/src/main/java/com/netflix/eureka/EurekaServerContextHolder.java
+++ eureka-core/src/main/java/com/netflix/eureka/EurekaServerContextHolder.java
@@ -42,6 +42,9 @@ public class EurekaServerContextHolder {
   }
 
   public static EurekaServerContextHolder getInstance() {
+    if (holder == null) {
+      throw new IllegalStateException("EurekaServerContextHolder has not been initialized.");
+    }
     return holder;
   }
 }
