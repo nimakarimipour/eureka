fefe4bb2f9d839cf5b693fbf197c22e90abc78e9
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 16:52:55 -0700

    fix: 6 - DEREFERENCE_NULLABLE - resourceRecordSetWithHostedZone - dereferenced expression resourceRecordSetWithHostedZone is @Nullable

--- eureka-core/src/main/java/com/netflix/eureka/aws/Route53Binder.java
+++ eureka-core/src/main/java/com/netflix/eureka/aws/Route53Binder.java
@@ -270,7 +270,8 @@ public class Route53Binder implements AwsBinder {
   private void unbindFromDomain(String domain) throws InterruptedException {
     ResourceRecordSetWithHostedZone resourceRecordSetWithHostedZone =
         getResourceRecordSetWithHostedZone(domain);
-    if (hasValue(resourceRecordSetWithHostedZone, registrationHostname)) {
+    if (resourceRecordSetWithHostedZone != null
+        && hasValue(resourceRecordSetWithHostedZone, registrationHostname)) {
       resourceRecordSetWithHostedZone
           .getResourceRecordSet()
           .getResourceRecords()
