52a086e426c485546378cbfe321cce93ace8bd4f
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-02 01:27:59 -0700

    Revert "fix: 24 - DEREFERENCE_NULLABLE - existingLease.getHolder().getStatus().name(), - dereferenced expression existingLease is @Nullable"
    This reverts commit 777fe45e41c6a249e3862effcbad99f0431086ad.

--- eureka-core/src/main/java/com/netflix/eureka/registry/rule/LeaseExistsRule.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/rule/LeaseExistsRule.java
@@ -2,6 +2,7 @@ package com.netflix.eureka.registry.rule;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.eureka.lease.Lease;
+import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,22 +17,25 @@ public class LeaseExistsRule implements InstanceStatusOverrideRule {
 
   @Override
   public StatusOverrideResult apply(
-      InstanceInfo instanceInfo, Lease<InstanceInfo> existingLease, boolean isReplication) {
+      InstanceInfo instanceInfo,
+      @Nullable Lease<InstanceInfo> existingLease,
+      boolean isReplication) {
+    // This is for backward compatibility until all applications have ASG
+    // names, otherwise while starting up
+    // the client status may override status replicated from other servers
     if (!isReplication) {
       InstanceInfo.InstanceStatus existingStatus = null;
       if (existingLease != null) {
         existingStatus = existingLease.getHolder().getStatus();
       }
+      // Allow server to have its way when the status is UP or OUT_OF_SERVICE
       if ((existingStatus != null)
           && (InstanceInfo.InstanceStatus.OUT_OF_SERVICE.equals(existingStatus)
               || InstanceInfo.InstanceStatus.UP.equals(existingStatus))) {
         logger.debug(
             "There is already an existing lease with status {}  for instance {}",
-            NullabilityUtil.castToNonnull(existingLease, "checked for null")
-                .getHolder()
-                .getStatus()
-                .name(),
-            NullabilityUtil.castToNonnull(existingLease, "checked for null").getHolder().getId());
+            existingLease.getHolder().getStatus().name(),
+            existingLease.getHolder().getId());
         return StatusOverrideResult.matchingStatus(existingLease.getHolder().getStatus());
       }
     }
