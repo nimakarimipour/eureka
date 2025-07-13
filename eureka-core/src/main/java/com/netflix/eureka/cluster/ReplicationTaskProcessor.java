421d251c43c2d39bb0b9c95133523f0eb3be2884
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:36:12 -0700

    fix: 18 - DEREFERENCE_NULLABLE - String message = e.getMessage().toLowerCase(); - dereferenced expression e.getMessage() is @Nullable

--- eureka-core/src/main/java/com/netflix/eureka/cluster/ReplicationTaskProcessor.java
+++ eureka-core/src/main/java/com/netflix/eureka/cluster/ReplicationTaskProcessor.java
@@ -10,13 +10,14 @@ import com.netflix.eureka.cluster.protocol.ReplicationInstanceResponse;
 import com.netflix.eureka.cluster.protocol.ReplicationList;
 import com.netflix.eureka.cluster.protocol.ReplicationListResponse;
 import com.netflix.eureka.util.batcher.TaskProcessor;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.IOException;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.Nullable;
 
 /**
  * @author Tomasz Bak
@@ -214,7 +215,8 @@ class ReplicationTaskProcessor implements TaskProcessor<ReplicationTask> {
   private static boolean maybeReadTimeOut(Throwable e) {
     do {
       if (IOException.class.isInstance(e)) {
-        String message = e.getMessage().toLowerCase();
+        String message = Nullability.castToNonnull(e.getMessage(), "message is checked");
+        message = message.toLowerCase();
         Matcher matcher = READ_TIME_OUT_PATTERN.matcher(message);
         if (matcher.find()) {
           return true;
