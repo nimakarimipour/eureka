5d5f06fa0ab5fbfc6eff2260b8196664fc4eb7df
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 00:30:25 -0700

    before:11to10 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression applications is @Nullable' /home/nima/Developer/nullness-benchmarks/eureka/eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java:953           for (Application application : applications.getRegisteredApplications()) {

--- eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
@@ -920,57 +920,59 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
    *     region.
    */
   @Deprecated
-  public Applications getApplicationDeltas() {
-    GET_ALL_CACHE_MISS_DELTA.increment();
-    Applications apps = new Applications();
-    apps.setVersion(responseCache.getVersionDelta().get());
-    Map<String, Application> applicationInstancesMap = new HashMap<String, Application>();
-    write.lock();
-    try {
-      Iterator<RecentlyChangedItem> iter = this.recentlyChangedQueue.iterator();
-      logger.debug(
-          "The number of elements in the delta queue is : {}", this.recentlyChangedQueue.size());
-      while (iter.hasNext()) {
-        Lease<InstanceInfo> lease = iter.next().getLeaseInfo();
-        InstanceInfo instanceInfo = lease.getHolder();
+    public Applications getApplicationDeltas() {
+      GET_ALL_CACHE_MISS_DELTA.increment();
+      Applications apps = new Applications();
+      apps.setVersion(responseCache.getVersionDelta().get());
+      Map<String, Application> applicationInstancesMap = new HashMap<String, Application>();
+      write.lock();
+      try {
+        Iterator<RecentlyChangedItem> iter = this.recentlyChangedQueue.iterator();
         logger.debug(
-            "The instance id {} is found with status {} and actiontype {}",
-            instanceInfo.getId(),
-            instanceInfo.getStatus().name(),
-            instanceInfo.getActionType().name());
-        Application app = applicationInstancesMap.get(instanceInfo.getAppName());
-        if (app == null) {
-          app = new Application(instanceInfo.getAppName());
-          applicationInstancesMap.put(instanceInfo.getAppName(), app);
-          apps.addApplication(app);
+            "The number of elements in the delta queue is : {}", this.recentlyChangedQueue.size());
+        while (iter.hasNext()) {
+          Lease<InstanceInfo> lease = iter.next().getLeaseInfo();
+          InstanceInfo instanceInfo = lease.getHolder();
+          logger.debug(
+              "The instance id {} is found with status {} and actiontype {}",
+              instanceInfo.getId(),
+              instanceInfo.getStatus().name(),
+              instanceInfo.getActionType().name());
+          Application app = applicationInstancesMap.get(instanceInfo.getAppName());
+          if (app == null) {
+            app = new Application(instanceInfo.getAppName());
+            applicationInstancesMap.put(instanceInfo.getAppName(), app);
+            apps.addApplication(app);
+          }
+          app.addInstance(new InstanceInfo(decorateInstanceInfo(lease)));
         }
-        app.addInstance(new InstanceInfo(decorateInstanceInfo(lease)));
-      }
-
-      boolean disableTransparentFallback = serverConfig.disableTransparentFallbackToOtherRegion();
-
-      if (!disableTransparentFallback) {
-        Applications allAppsInLocalRegion = getApplications(false);
-
-        for (RemoteRegionRegistry remoteRegistry : this.regionNameVSRemoteRegistry.values()) {
-          Applications applications = remoteRegistry.getApplicationDeltas();
-          for (Application application : applications.getRegisteredApplications()) {
-            Application appInLocalRegistry =
-                allAppsInLocalRegion.getRegisteredApplications(application.getName());
-            if (appInLocalRegistry == null) {
-              apps.addApplication(application);
+  
+        boolean disableTransparentFallback = serverConfig.disableTransparentFallbackToOtherRegion();
+  
+        if (!disableTransparentFallback) {
+          Applications allAppsInLocalRegion = getApplications(false);
+  
+          for (RemoteRegionRegistry remoteRegistry : this.regionNameVSRemoteRegistry.values()) {
+            Applications applications = remoteRegistry.getApplicationDeltas();
+            if (applications != null) { // Check for null before dereference
+              for (Application application : applications.getRegisteredApplications()) {
+                Application appInLocalRegistry =
+                    allAppsInLocalRegion.getRegisteredApplications(application.getName());
+                if (appInLocalRegistry == null) {
+                  apps.addApplication(application);
+                }
+              }
             }
           }
         }
+  
+        Applications allApps = getApplications(!disableTransparentFallback);
+        apps.setAppsHashCode(allApps.getReconcileHashCode());
+        return apps;
+      } finally {
+        write.unlock();
       }
-
-      Applications allApps = getApplications(!disableTransparentFallback);
-      apps.setAppsHashCode(allApps.getReconcileHashCode());
-      return apps;
-    } finally {
-      write.unlock();
     }
-  }
 
   /**
    * Gets the application delta also including instances from the passed remote regions, with the
