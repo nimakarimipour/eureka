6cbdd655ee07f0fd8fd61f93c328817396b00a9d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:34:29 -0700

    Revert "fix: 3 - DEREFERENCE_NULLABLE - overriddenInstanceStatus.name(), - dereferenced expression overriddenInstanceStatus is @Nullable"
    This reverts commit e092a1af2baf97369d8632a089d995206ead2950.

--- eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
@@ -390,8 +390,7 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
         // touchASGCache(instanceInfo.getASGName());
         InstanceStatus overriddenInstanceStatus =
             this.getOverriddenInstanceStatus(instanceInfo, leaseToRenew, isReplication);
-        if (overriddenInstanceStatus == null
-            || overriddenInstanceStatus == InstanceStatus.UNKNOWN) {
+        if (overriddenInstanceStatus == InstanceStatus.UNKNOWN) {
           logger.info(
               "Instance status UNKNOWN possibly due to deleted override for instance {}"
                   + "; re-register required",
@@ -683,8 +682,7 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
    * @return the application
    * @see com.netflix.discovery.shared.LookupService#getApplication(java.lang.String)
    */
-  @Nullable
-  @Override
+  @Nullable @Override
   public Application getApplication(String appName) {
     boolean disableTransparentFallback = serverConfig.disableTransparentFallbackToOtherRegion();
     return this.getApplication(appName, !disableTransparentFallback);
@@ -699,8 +697,7 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
    *     EurekaServerConfig#getRemoteRegionUrls()}, false otherwise
    * @return the application
    */
-  @Nullable
-  @Override
+  @Nullable @Override
   public Application getApplication(String appName, boolean includeRemoteRegion) {
     Application app = null;
 
@@ -1389,8 +1386,7 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
    */
   protected abstract InstanceStatusOverrideRule getInstanceInfoOverrideRule();
 
-  @Nullable
-  protected InstanceInfo.InstanceStatus getOverriddenInstanceStatus(
+  @Nullable protected InstanceInfo.InstanceStatus getOverriddenInstanceStatus(
       InstanceInfo r, @Nullable Lease<InstanceInfo> existingLease, boolean isReplication) {
     InstanceStatusOverrideRule rule = getInstanceInfoOverrideRule();
     logger.debug("Processing override status using rule: {}", rule);
