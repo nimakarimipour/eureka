eb1d0d9f771aaef65ef0a0e98eda803d30872cc2
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:35:18 -0700

    Revert "fix: 10 - DEREFERENCE_NULLABLE - regions = regionsStr.toLowerCase().split(","); - dereferenced expression regionsStr is @Nullable"
    This reverts commit ace208e1c3db9fb4fed86da95eaa7f65dcb88c1e.

--- eureka-core/src/main/java/com/netflix/eureka/resources/ApplicationsResource.java
+++ eureka-core/src/main/java/com/netflix/eureka/resources/ApplicationsResource.java
@@ -28,7 +28,6 @@ import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
 import com.netflix.eureka.registry.ResponseCache;
 import com.netflix.eureka.registry.ResponseCacheImpl;
 import com.netflix.eureka.util.EurekaMonitors;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Arrays;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
@@ -217,7 +216,7 @@ public class ApplicationsResource {
     if (!isRemoteRegionRequested) {
       EurekaMonitors.GET_ALL_DELTA.increment();
     } else {
-      regions = Nullability.castToNonnull(regionsStr).toLowerCase().split(",");
+      regions = regionsStr.toLowerCase().split(",");
       Arrays.sort(
           regions); // So we don't have different caches for same regions queried in different
       // order.
