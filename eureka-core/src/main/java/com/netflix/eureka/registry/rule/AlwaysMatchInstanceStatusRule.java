e092a1af2baf97369d8632a089d995206ead2950
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:34:27 -0700

    fix: 3 - DEREFERENCE_NULLABLE - overriddenInstanceStatus.name(), - dereferenced expression overriddenInstanceStatus is @Nullable

--- eureka-core/src/main/java/com/netflix/eureka/registry/rule/AlwaysMatchInstanceStatusRule.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/rule/AlwaysMatchInstanceStatusRule.java
@@ -2,9 +2,9 @@ package com.netflix.eureka.registry.rule;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.eureka.lease.Lease;
+import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.Nullable;
 
 /**
  * This rule matches always and returns the current status of the instance.
@@ -16,7 +16,9 @@ public class AlwaysMatchInstanceStatusRule implements InstanceStatusOverrideRule
 
   @Override
   public StatusOverrideResult apply(
-      InstanceInfo instanceInfo, @Nullable Lease<InstanceInfo> existingLease, boolean isReplication) {
+      InstanceInfo instanceInfo,
+      @Nullable Lease<InstanceInfo> existingLease,
+      boolean isReplication) {
     logger.debug(
         "Returning the default instance status {} for instance {}",
         instanceInfo.getStatus(),
