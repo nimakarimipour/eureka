d86572741905a56d7d5cc6ad59c800c6573134d8
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:34:47 -0700

    Revert "fix: 6 - METHOD_NO_INIT - public RateLimitingFilter() {} - initializer method does not guarantee @NonNull field serverConfig (line 107) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 683c1192393054f86110d0bf0e4d93463f5e349d.

--- eureka-core/src/main/java/com/netflix/eureka/RateLimitingFilter.java
+++ eureka-core/src/main/java/com/netflix/eureka/RateLimitingFilter.java
@@ -20,7 +20,6 @@ import com.netflix.appinfo.AbstractEurekaIdentity;
 import com.netflix.appinfo.EurekaClientIdentity;
 import com.netflix.discovery.util.RateLimiter;
 import com.netflix.eureka.util.EurekaMonitors;
-import com.uber.nullaway.annotations.Initializer;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -40,6 +39,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import javax.annotation.Nullable;
 
 /**
  * Rate limiting filter, with configurable threshold above which non-privileged clients will be
@@ -104,7 +104,6 @@ public class RateLimitingFilter implements Filter {
   /** Only full registry fetches. */
   private static final RateLimiter registryFullFetchRateLimiter = new RateLimiter(TimeUnit.SECONDS);
 
-  @SuppressWarnings("NullAway.Init")
   private EurekaServerConfig serverConfig;
 
   @Inject
@@ -115,7 +114,6 @@ public class RateLimitingFilter implements Filter {
   // for non-DI use
   public RateLimitingFilter() {}
 
-  @Initializer
   @Override
   public void init(FilterConfig filterConfig) throws ServletException {
     if (serverConfig == null) {
