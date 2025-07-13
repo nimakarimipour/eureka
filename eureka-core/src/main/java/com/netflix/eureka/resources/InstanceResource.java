45247c70fdafbe6247b241cad7dcbde30d4bd675
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:34:55 -0700

    Revert "fix: 7 - DEREFERENCE_NULLABLE - resourceRecordSetWithHostedZone - dereferenced expression resourceRecordSetWithHostedZone is @Nullable"
    This reverts commit 94f6c5c2d8781a8cd7294f518d935ac42e2b19ab.

--- eureka-core/src/main/java/com/netflix/eureka/resources/InstanceResource.java
+++ eureka-core/src/main/java/com/netflix/eureka/resources/InstanceResource.java
@@ -27,7 +27,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import javax.annotation.Nullable;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -42,6 +41,7 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import javax.annotation.Nullable;
 
 /**
  * A <em>jersey</em> resource that handles operations for a particular instance.
