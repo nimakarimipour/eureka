cfd3bbf4b23ff9de1c2cc33cf103cf10169c209b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 17:05:24 -0700

    fix: 15 - DEREFERENCE_NULLABLE - return this.applications.get().getRegisteredApplications(appName); - dereferenced expression this.applications.get() is @Nullable

--- eureka-core/src/main/java/com/netflix/eureka/registry/RemoteRegionRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/RemoteRegionRegistry.java
@@ -57,11 +57,11 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.Nullable;
 
 /**
  * Handles all registry operations that needs to be done on a eureka service running in an other
@@ -416,7 +416,8 @@ public class RemoteRegionRegistry implements LookupService<String> {
    * @param delta - true, if the fetch needs to get deltas, false otherwise
    * @return - response which has information about the data.
    */
-  @Nullable private Applications fetchRemoteRegistry(boolean delta) {
+  @Nullable
+  private Applications fetchRemoteRegistry(boolean delta) {
     logger.info(
         "Getting instance registry info from the eureka server : {} , delta : {}",
         this.remoteRegionURL,
@@ -512,14 +513,19 @@ public class RemoteRegionRegistry implements LookupService<String> {
     return applications.get();
   }
 
-  @Nullable @Override
+  @Nullable
+  @Override
   public InstanceInfo getNextServerFromEureka(String arg0, boolean arg1) {
     return null;
   }
 
   @Override
   public Application getApplication(String appName) {
-    return this.applications.get().getRegisteredApplications(appName);
+    Applications apps = this.applications.get();
+    if (apps == null) {
+      return null;
+    }
+    return apps.getRegisteredApplications(appName);
   }
 
   @Override
@@ -536,7 +542,8 @@ public class RemoteRegionRegistry implements LookupService<String> {
     return Collections.emptyList();
   }
 
-  @Nullable public Applications getApplicationDeltas() {
+  @Nullable
+  public Applications getApplicationDeltas() {
     return this.applicationsDelta.get();
   }
 
