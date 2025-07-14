ac532b38cb976140b7a8d69877a274b183995683
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 17:04:39 -0700

    fix: 7 - DEREFERENCE_NULLABLE - resourceRecordSetWithHostedZone - dereferenced expression resourceRecordSetWithHostedZone is @Nullable

--- eureka-core/src/main/java/com/netflix/eureka/resources/AbstractVIPResource.java
+++ eureka-core/src/main/java/com/netflix/eureka/resources/AbstractVIPResource.java
@@ -26,7 +26,6 @@ import com.netflix.eureka.registry.ResponseCache;
 import javax.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.Nullable;
 
 /**
  * Abstract class for the common functionality of a VIP/SVIP resource.
