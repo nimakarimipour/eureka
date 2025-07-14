1582f9978e349c0d940e3011647a9cfcb03bf5f4
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 17:05:13 -0700

    Revert "fix: 13 - DEREFERENCE_NULLABLE - if (serverConfig.shouldLogIdentityHeaders()) { - dereferenced expression serverConfig is @Nullable"
    This reverts commit c0fac4face07bd63b07f88dee7d1075a5588259a.

--- eureka-core/src/main/java/com/netflix/eureka/ServerRequestAuthFilter.java
+++ eureka-core/src/main/java/com/netflix/eureka/ServerRequestAuthFilter.java
@@ -5,7 +5,6 @@ import com.netflix.appinfo.AbstractEurekaIdentity;
 import com.netflix.servo.monitor.DynamicCounter;
 import com.netflix.servo.monitor.MonitorConfig;
 import java.io.IOException;
-import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.servlet.Filter;
@@ -15,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.annotation.Nullable;
 
 /**
  * An auth filter for client requests. For now, it only logs supported client identification data
@@ -59,7 +59,7 @@ public class ServerRequestAuthFilter implements Filter {
   }
 
   protected void logAuth(ServletRequest request) {
-    if (serverConfig != null && serverConfig.shouldLogIdentityHeaders()) {
+    if (serverConfig.shouldLogIdentityHeaders()) {
       if (request instanceof HttpServletRequest) {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
 
--- eureka-core/src/main/java/com/netflix/eureka/cluster/protocol/ReplicationInstance.java
+++ eureka-core/src/main/java/com/netflix/eureka/cluster/protocol/ReplicationInstance.java
@@ -46,13 +46,11 @@ public class ReplicationInstance {
     return lastDirtyTimestamp;
   }
 
-  @Nullable
-  public String getOverriddenStatus() {
+  @Nullable public String getOverriddenStatus() {
     return overriddenStatus;
   }
 
-  @Nullable
-  public String getStatus() {
+  @Nullable public String getStatus() {
     return status;
   }
 
