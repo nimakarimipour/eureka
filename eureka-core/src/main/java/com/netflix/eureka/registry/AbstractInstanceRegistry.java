6493837f29226a050db25f8146b251a7994d8cf6
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 17:05:50 -0700

    Revert "fix: 19 - METHOD_NO_INIT - protected AbstractInstanceRegistry( - initializer method does not guarantee @NonNull field responseCache (line 111) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 95ea1f7d48ebf1301b52db7c52d927579c46144d.

--- eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
+++ eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java
@@ -125,8 +125,6 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
         getDeltaRetentionTask(),
         serverConfig.getDeltaRetentionTimerIntervalInMs(),
         serverConfig.getDeltaRetentionTimerIntervalInMs());
-
-    this.responseCache = new ResponseCacheImpl(serverConfig, serverCodecs, this);
   }
 
   @Override
@@ -684,8 +682,7 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
    * @return the application
    * @see com.netflix.discovery.shared.LookupService#getApplication(java.lang.String)
    */
-  @Nullable
-  @Override
+  @Nullable @Override
   public Application getApplication(String appName) {
     boolean disableTransparentFallback = serverConfig.disableTransparentFallbackToOtherRegion();
     return this.getApplication(appName, !disableTransparentFallback);
@@ -700,8 +697,7 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
    *     EurekaServerConfig#getRemoteRegionUrls()}, false otherwise
    * @return the application
    */
-  @Nullable
-  @Override
+  @Nullable @Override
   public Application getApplication(String appName, boolean includeRemoteRegion) {
     Application app = null;
 
@@ -1390,8 +1386,7 @@ public abstract class AbstractInstanceRegistry implements InstanceRegistry {
    */
   protected abstract InstanceStatusOverrideRule getInstanceInfoOverrideRule();
 
-  @Nullable
-  protected InstanceInfo.InstanceStatus getOverriddenInstanceStatus(
+  @Nullable protected InstanceInfo.InstanceStatus getOverriddenInstanceStatus(
       InstanceInfo r, @Nullable Lease<InstanceInfo> existingLease, boolean isReplication) {
     InstanceStatusOverrideRule rule = getInstanceInfoOverrideRule();
     logger.debug("Processing override status using rule: {}", rule);
